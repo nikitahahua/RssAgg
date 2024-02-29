3b744c5bcb0a2dc8cb493018bfa5ccc872f70a7a
nikitagagua <2gagua121@gmail.com>
2024-02-29 17:29:13 +0200

    feat: add running server

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/nikitahahua/RssAgg
+
+go 1.21.4
+
+require (
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+)
--- main.go
+++ main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
 	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	log.SetPrefix("georgian greetings: ")
 	log.SetFlags(0)
-	//Load()
+	godotenv.Load(".env")
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatalln("cant connect to the port")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server starting on pory %v", port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 	fmt.Println("port: " + port)
