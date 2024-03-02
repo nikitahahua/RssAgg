7dbe5db5fac0a974719734eb84847fc73709e2d5
nikitagagua <2gagua121@gmail.com>
2024-03-02 23:47:21 +0200

    dev: add models, update env

--- main.go
+++ main.go
@@ -1,36 +1,102 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/nikitahahua/RssAgg/internal/database"
 	"log"
 	"net/http"
 	"os"
 )
 
+type apiConfig struct {
+	DB *database.Queries
+}
+
 func main() {
+
 	log.SetPrefix("georgian greetings: ")
 	log.SetFlags(0)
+
 	godotenv.Load(".env")
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatalln("cant connect to the port")
+		log.Fatal("PORT environment variable is not set")
 	}
+
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("DB_URL environment variable is not set")
+	}
+
+	connection, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatal("cant connect to the data base by url : \n" + dbUrl)
+	}
+
+	apiCfg := apiConfig{DB: database.New(connection)}
 
 	router := chi.NewRouter()
 
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get(
+		"/healthz",
+		handleRequest,
+	)
+	v1Router.Get(
+		"/err",
+		handlerErr,
+	)
+	v1Router.Get(
+		"/users",
+		apiCfg.middlewareAuth(apiCfg.handleGetUser),
+	)
+	v1Router.Post(
+		"/users",
+		apiCfg.handlerCreateUser,
+	)
+
+	v1Router.Post(
+		"/feeds",
+		apiCfg.middlewareAuth(apiCfg.handlerCreateFeed),
+	)
+	v1Router.Get(
+		"/feeds",
+		apiCfg.handlerGetFeeds,
+	)
+	v1Router.Post(
+		"/feed_follows",
+		apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollows),
+	)
+	v1Router.Get(
+		"/feed_follows",
+		apiCfg.middlewareAuth(apiCfg.handlerFeedFollowsGet),
+	)
+	v1Router.Delete(
+		"/feed_follows/{feed_follow_id}",
+		apiCfg.middlewareAuth(apiCfg.handlerDeleteFeedFollow),
+	)
+
+	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Handler: router,
 		Addr:    ":" + port,
+		Handler: router,
 	}
 
-	log.Printf("Server starting on pory %v", port)
-	err := srv.ListenAndServe()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	fmt.Println("port: " + port)
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
---
+++ models/models.go
@@ -0,0 +1,80 @@
+package models
+
+import (
+	"github.com/nikitahahua/RssAgg/internal/database"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	APIKey    string    `json:"api-key"`
+}
+
+func GetUserFromDb(dbUser database.User) User {
+	return User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Name:      dbUser.Name,
+		APIKey:    dbUser.ApiKey,
+	}
+}
+
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	Url       string    `json:"url"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func GetFeedFromDb(feed database.Feed) Feed {
+	return Feed{
+		ID:        feed.ID,
+		CreatedAt: feed.CreatedAt,
+		UpdatedAt: feed.UpdatedAt,
+		Name:      feed.Name,
+		Url:       feed.Url,
+		UserID:    feed.UserID,
+	}
+}
+
+func GetAllFeedsFromDb(feeds []database.Feed) []Feed {
+	result := make([]Feed, len(feeds))
+	for i, feed := range feeds {
+		result[i] = GetFeedFromDb(feed)
+	}
+	return result
+}
+
+type FeedFollow struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	UserID    uuid.UUID `json:"user_id"`
+	FeedID    uuid.UUID `json:"feed_id"`
+}
+
+func GetFeedFollowsFromDb(feedFollow database.FeedFollow) FeedFollow {
+	return FeedFollow{
+		ID:        feedFollow.ID,
+		CreatedAt: feedFollow.CreatedAt,
+		UpdatedAt: feedFollow.UpdatedAt,
+		UserID:    feedFollow.UserID,
+		FeedID:    feedFollow.FeedID,
+	}
+}
+
+func GetAllFollowsFromDb(feedFollows []database.FeedFollow) []FeedFollow {
+	result := make([]FeedFollow, len(feedFollows))
+	for i, feedFollow := range feedFollows {
+		result[i] = GetFeedFollowsFromDb(feedFollow)
+	}
+	return result
+}
