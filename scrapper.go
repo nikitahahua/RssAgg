d2844170f91681af59d7b158c7ba9b4a957b06ae
nikitagagua <2gagua121@gmail.com>
2024-03-03 15:28:22 +0200

    feat: add logic for getting post by rss link

---
+++ scrapper.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"github.com/google/uuid"
+	"github.com/nikitahahua/RssAgg/internal/database"
+	"log"
+	"strings"
+	"sync"
+	"time"
+)
+
+func startScraping(db *database.Queries, concurrency int, timeBetweenRequest time.Duration) {
+	log.Printf("Collecting feeds every %s on %v goroutines...", timeBetweenRequest, concurrency)
+	ticker := time.NewTicker(timeBetweenRequest)
+
+	for ; ; <-ticker.C {
+		feeds, err := db.GetNextFeedsToFetch(context.Background(), int32(concurrency))
+		if err != nil {
+			log.Println("Couldn't get next feeds to fetch", err)
+			continue
+		}
+		log.Printf("Found %v feeds to fetch!", len(feeds))
+
+		wg := &sync.WaitGroup{}
+		for _, feed := range feeds {
+			wg.Add(1)
+			go scrapeFeed(db, wg, feed)
+		}
+		wg.Wait()
+	}
+}
+
+func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
+	defer wg.Done()
+	_, err := db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Printf("Couldn't mark feed %s fetched: %v", feed.Name, err)
+		return
+	}
+
+	feedData, err := fetchFeed(feed.Url)
+	if err != nil {
+		log.Printf("Couldn't collect feed %s: %v", feed.Name, err)
+		return
+	}
+	for _, item := range feedData.Channel.Item {
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubTime, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Fatal("couldn't parse the time of post")
+			continue
+		}
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description.String,
+			PublishedAt: pubTime,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Fatal("failed to create the post")
+		}
+		log.Printf("Feed %s collected, %v posts found", feed.Name, len(feedData.Channel.Item))
+	}
+}
