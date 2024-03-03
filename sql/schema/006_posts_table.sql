8342675399d11cb26e2b4cdb9b6044c9b2fbefeb
nikitagagua <2gagua121@gmail.com>
2024-03-03 15:26:52 +0200

    fear: add post tables and queries for tables

---
+++ sql/schema/006_posts_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+
+CREATE TABLE posts
+(
+    id           UUID PRIMARY KEY,
+    created_at   TIMESTAMP NOT NULL,
+    updated_at   TIMESTAMP NOT NULL,
+    title        TEXT      NOT NULL,
+    description  TEXT NOT NULL,
+    published_at TIMESTAMP NOT NULL,
+    url          TEXT      not null,
+    feed_id      UUID      NOT NULL REFERENCES feeds (id) ON DELETE CASCADE
+);
+
+-- +goose Down
+
+DROP TABLE posts;
